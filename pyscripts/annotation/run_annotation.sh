98e04dd802e9e54572f54739e66d523064bf7704
Brian Yee <bay001@tscc-1-48.sdsc.edu>
2017-06-20 10:41:22 -0700

    run_annotation runscript

---
+++ pyscripts/annotation/run_annotation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python annotate.py \
+--input /home/bay001/projects/codebase/bfx/pyscripts/data/WTV5_02_01.basedon_WTV5_02_01.peaks.l2inputnormnew.bed.compressed.chr7.bed \
+--output /home/bay001/projects/codebase/bfx/pyscripts/data/WTV5_02_01.basedon_WTV5_02_01.peaks.l2inputnormnew.bed.compressed.chr7.bed.brianannotated \
+--gtfdb /projects/ps-yeolab3/bay001/annotations/gencode.v19.annotation.gtf.db
